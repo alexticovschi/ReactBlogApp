6fc2078ce7c7f42e7aa4d81c5013342182dd7673
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-23 19:13:24 +0000

    defining first route

--- src/index.js
+++ src/index.js
@@ -4,31 +4,22 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-  render() {
-    return <div>Hello!</div>
-  }
-}
-
-class Goodbye extends React.Component {
-  render() {
-    return <div>Goodbye!</div>
-  }
-}
 
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-    <div>
-      <Route path="/hello" component={Hello} />
-      <Route path="/goodbye" component={Goodbye} />
-    </div>
+      <div>
+        {/* Whenever evry user goes to the path of just slash
+          show the component PostsIndex */}
+        <Route path="/" component={PostsIndex} />
+      </div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
