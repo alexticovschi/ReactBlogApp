2e51fc60213f5acaf5c69fe5cb310275db56752d
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-27 11:35:22 +0000

    fix react router bug using Switch component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 
@@ -19,10 +19,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        {/* Whenever evry user goes to the path of just slash
-          show the component PostsIndex */}
-        <Route path="/" component={PostsIndex} />
-        <Route path="/posts/new" component={PostsNew} />        
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />  
+          <Route path="/" component={PostsIndex} />          
+        </Switch>     
       </div>
     </BrowserRouter>
   </Provider>
