2548c2b58d1e2b77dc8762a81f77e01fc2801d5f
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-28 15:21:30 +0000

    setting up Redux Form and Field component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderTitleField(field) {
+    return (
+      <div>
+        <input 
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        PostsNew!
+        <form>
+          {/* the Field component is responsible for event handling and updating our different pieces of state */}
+          <Field 
+            name="title"
+            component={this.renderTitleField}
+          />
+        </form>
       </div>
     );
   }
 }
 
 
-export default PostsNew;
+export default reduxForm({ form: 'PostsNewForm' })(PostsNew);
