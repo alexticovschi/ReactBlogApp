f76c211eb69975c5da36704fc79182e0b1ecad83
AlexTicovschi <alexmticovschi@gmail.com>
2018-02-04 10:20:14 +0000

    receiving new posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST  = 'fetch_post';
 export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -33,6 +34,16 @@ export function createPost(values, callback) {
     .then(() => callback());
   return {
     type: CREATE_POST,
+    payload: request
+  }
+}
+
+// this action creator is going to be called with the ID of the post that we want to fetch
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: request
   }
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,23 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // // take the newState object and add on additional property of the post ID and set that equal to the post
+      // newState[post.id] = post;
+      // return newState;
+
+      // Key interpolation
+      // Make a new key on this object using the value of action.payload.data.id and 
+      //   set its value equal to action.payload.data
+      // As a user starts to fetch more show routes inside the application, fetch each 
+      //   additional posts and add them into the overall state object
+      return { ...state, [action.payload.data.id]: action.payload.data };
+
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default: 
